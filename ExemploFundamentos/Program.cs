b46598aa825d88e1059fbf41c99656ec42cd3255
awmarques <139471143+awmarques@users.noreply.github.com>
2023-12-04 14:54:41 -0300

    commit

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using ExemploFundamentos.Models;
+
 
 // string apresentacao = "Olá, seja bem vindo!";
 // int quantidade = 1;
@@ -17,11 +19,99 @@ using ExemploFundamentos.Models;
 // Console.WriteLine("Valor da variavel condicao: " + condicao);
 
 
-DateTime dataAtual = DateTime.Now.AddYears(1000);
-Console.WriteLine(dataAtual.ToString("dd/mm/yy hh:mm"));
+// DateTime dataAtual = DateTime.Now.AddYears(1000);
+// Console.WriteLine(dataAtual.ToString("dd/mm/yy hh:mm"));
 
-Pessoa p = new Pessoa();
-p.nome = "andrew";
-p.idade = 32;
+// Pessoa p = new Pessoa();
+// p.nome = "andrew";
+//  p.idade = 32;
 
-p.Apresentar();
+// int a = 1;
+// int b = 2;
+// int c = a + b;
+
+// c = c +5;
+// c +=5;
+
+// Console.WriteLine(c);
+
+// p.Apresentar();
+
+
+//conversao variaveis -> Cast
+// converte um tipo para outro
+//operacao que apresenta um certo risco
+//porque se o valor da variavel aser convertido for //invalido
+// Convert trata nulo e atribui zero
+//Parse não aceita nulo e da erro no programa
+// String a = "55555";
+// int b = Convert.ToInt32(a);
+// Console.WriteLine(b);
+
+// string teste = "132";
+// int top = int.Parse(teste);
+// Console.WriteLine(top);
+// // cast inteiro para string
+// int x = 123453;
+// String texto = x.ToString();
+// Console.WriteLine(texto);
+// texto = Convert.ToString(x);
+// Console.WriteLine(texto.ToString());
+// Console.WriteLine(texto);
+
+//cast implicito
+//overflow exception
+// long a = long.MaxValue;
+// int b  = Convert.ToInt32(a);
+// Console.WriteLine("tatata" + b);
+
+// String texto = "123 -a";
+// int b=10;
+// int.TryParse(texto, out b);
+// Console.WriteLine("" + texto);
+
+
+
+
+// int quantidadadeEmEstoque = 10;
+// int quantidadeCompra = 0;
+
+// bool possivelVenda = quantidadeCompra > 0 && quantidadadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra:{quantidadeCompra}");
+// Console.WriteLine($"É possível realizar venda? {possivelVenda}");
+
+// if (quantidadeCompra==0){
+//     Console.WriteLine("Quantidade invalida.");
+
+// } else if(possivelVenda){
+//     Console.WriteLine("Venda realizada.");
+// } else {
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada.");
+// }
+
+
+
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+// if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+//     Console.WriteLine("Vogal");
+// } else {
+//     Console.WriteLine("Consoante");
+// }
+
+switch (letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal"); 
+        break;
+
+    default:
+        Console.WriteLine("Consoante");
+        break;
+}
