8ae35a264b7c01b261f33618bf3c55b8995f0d73
awmarques <139471143+awmarques@users.noreply.github.com>
2023-12-11 08:22:48 -0300

    bin

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -7,6 +7,12 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        
+        public string nomePessoa { get; set; }
+        public int idadePessoa { get; set; }
+
+        public void Apresentar (){
+            Console.WriteLine($"Olá, sou uma Pessoa. Meu nome é: {nomePessoa} e tenho {idadePessoa} anos de idade!");
+
+        }
     }
 }
--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+//Antes de usar uma classe é preciso estanciar uma variavel referente esta classe
+//A classe é só um modelo
+//classe| var = nova classe -> isso quer dizer que a variavel pessoa foi criada com base no modelo definido na 
+Pessoa pessoa = new Pessoa();
+//após instanciar na memória uma variavel referente o modelo estabelecido na classe Pessoa.
+//podemos utilizar as propriedades desse modelo, como nome da pessoa e idade por exemplo
+pessoa.nomePessoa = "João";
+pessoa.idadePessoa = 30;
+pessoa.Apresentar();
+
