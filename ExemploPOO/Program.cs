04f2dbec54b0a2d9e9200c95f097b5681ca4f83e
Andrew Marques <andrewmarques@protonmail.com>
2023-12-19 08:54:07 -0300

    abstracao POO

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExemploPOO.Models;
+Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "joao";
+p1.Idade = 1;
+p1.Apresentar();
